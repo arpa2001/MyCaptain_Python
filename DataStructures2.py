3a4e313c9b8c14b521bb0d50af8db716489800ca
arpa2001 <arpajitofficial@gmail.com>
2021-04-01 11:53:43 +0530

    Changes Committed

--- DataStructures2.py
+++
@@ -1,7 +0,0 @@
-import os, mimetypes
-mimetypes.init()
-
-fnm = input("Input the Filename: ")
-nm, extnsn = os.path.splitext(fnm)
-etyp = mimetypes.types_map[extnsn]
-print(f"The extension of the file is: '{etyp}'")
