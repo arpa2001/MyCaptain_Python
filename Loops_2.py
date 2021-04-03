11ed17bf999710e1af1bac75367318852ddc505f
arpa2001 <arpajitofficial@gmail.com>
2021-04-03 22:30:14 +0530

    Changes Committed

--- Loops_2.py
+++ Loops_2.py
@@ -1,15 +1,31 @@
+def positivecal(listx):
+    listy = []
+    buff = ""
+    for i in listx:
+        if i == "[":
+            continue
+        if i == "]":
+            listy.append(int(buff))
+        if i == "," or i == " ":
+            listy.append(int(buff))
+            buff = ""
+            continue
+        buff += i
+    result = []
+    for j in listy:
+        if int(j) >= 0:
+            result.append(int(j))
+    return result
+
+
 list1 = list(input("Input: list1 = "))
-print("Output: "),
-for i1 in list1:
-    if int(i1) >= 0:
-        print(i1),
+print("Output: ", end="")
+for n in positivecal(list1):
+    if n != positivecal(list1)[0]:
+        print(",", end=" ")
+    print(n, end="")
 
 print("\n")
 
 list2 = list(input("Input: list2 = "))
-result = []
-print("Output: "),
-for i2 in list2:
-    if int(i2) >= 0:
-        result.append(i2)
-print(result)
+print(f"Output: {positivecal(list2)}")
