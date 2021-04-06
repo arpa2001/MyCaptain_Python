4d130f129c88c376520d986fa14c03aad841c82f
arpa2001 <arpajitofficial@gmail.com>
2021-04-06 11:32:36 +0530

    now able to write csv

--- School_admin_tool.py
+++ School_admin_tool.py
@@ -1,6 +1,14 @@
 import csv
 
 
+def entry2csv(listdata):
+    # enters list data to csv file
+    with open("student_data.csv", 'a', newline = '') as datafile:
+        entry = csv.writer(datafile)
+        if datafile.tell() == 0:
+            entry.writerow(['Name', 'Age', 'Ph No.', 'Email ID'])
+        entry.writerow(listdata)
+
 def lwrcase(txt):
     # converting in lowercase
     txt_l = ""
@@ -13,7 +21,7 @@ def lwrcase(txt):
 
 def numsuffix(num):
     # adding suffix to number
-    ns = ""
+    nS = ""
     n_l = num % 10
     if num % 100 >= 11 and num % 100 <= 19:
         nS = str(num)+"th"
@@ -27,45 +35,48 @@ def numsuffix(num):
         nS = str(num)+"th"
     return nS
 
-# Taking entries
-entr = True
-n = 1
-while(entr):
-    # filling info
-    print(f"\nEnter {numsuffix(n)} student's following information:")
-    s_name = input("\tName:\t\t")
-    s_age = input("\tAge:\t\t")
-    s_ph = input("\tPhone number:\t")
-    s_email = input("\tEmail ID:\t")
 
-    info_list = [s_name, s_age, s_ph, s_email]  # listing info
+if __name__ == '__main__':
+    # Taking entries
+    entr = True
+    n = 1
+    while(entr):
+        # filling info
+        print(f"\nEnter {numsuffix(int(n))} student's following information:")
+        s_name = input("\tName:\t\t")
+        s_age = input("\tAge:\t\t")
+        s_ph = input("\tPhone number:\t")
+        s_email = input("\tEmail ID:\t")
 
-    # condition to check entry
-    print("\nPlease verify the info:")
-    print(f"\tName:\t\t{info_list[0]}\n\tAge:\t\t{info_list[1]}\n\tPhone Number:\t{info_list[2]}\n\tEmail ID:\t{info_list[3]}")
-    try_chk = False
-    while(not try_chk):
-        chk = input("\nIs the entered info correct? (y/n): ")
-        chk = lwrcase(chk)
-        if chk == "y":
-            # condition to ask next entry
-            try_info = False
-            while(not try_info):
-                nN = n + 1
-                rep = input(f"\nWish to enter {numsuffix(nN)} student info? (y/n): ")
-                rep = lwrcase(rep)
-                if rep == "y":
-                    n += 1
-                    try_info = True
-                elif rep == "n":
-                    entr = False
-                    try_info = True
-                else:
-                    print("Wrong Input!")
-                    try_info = False
-            try_chk = True
-        elif chk == "n":            
-            try_chk = True
-        else:
-            print("Wrong Input!")
-            try_chk = False
+        info_list = [s_name, s_age, s_ph, s_email]  # listing info
+
+        # condition to check entry
+        print("\nPlease verify the info:")
+        print(f"\tName:\t\t{info_list[0]}\n\tAge:\t\t{info_list[1]}\n\tPhone Number:\t{info_list[2]}\n\tEmail ID:\t{info_list[3]}")
+        try_chk = False
+        while(not try_chk):
+            chk = input("\nIs the entered info correct? (y/n): ")
+            chk = lwrcase(chk)
+            if chk == "y":
+                entry2csv(info_list)
+                # condition to ask next entry
+                try_info = False
+                while(not try_info):
+                    nN = int(n + 1)
+                    rep = input(f"\nWish to enter {numsuffix(nN)} student's info? (y/n): ")
+                    rep = lwrcase(rep)
+                    if rep == "y":
+                        n += 1
+                        try_info = True
+                    elif rep == "n":
+                        entr = False
+                        try_info = True
+                    else:
+                        print("Wrong Input!")
+                        try_info = False
+                try_chk = True
+            elif chk == "n":            
+                try_chk = True
+            else:
+                print("Wrong Input!")
+                try_chk = False
